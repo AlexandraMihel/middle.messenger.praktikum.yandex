9318c5dac4773a34bf8e66b987540cb36574f0b9
AlexandraMihel <mihelaleksa@yandex.ru>
2024-07-14 14:23:17 +0400

    Update chat-avatar.tmpl.js

--- src/modules/chat/components/chat-avatar/chat-avatar.tmpl.js
+++ src/modules/chat/components/chat-avatar/chat-avatar.tmpl.js
@@ -1,5 +1,5 @@
 export const tmpl = `
     <div class="chat-avatar">
-        <imgz width="60px" height="60px" src="{{url}}" alt="Аватар" class="chat-avatar-img">
+        <img width="60px" height="60px" src="{{url}}" alt="Аватар" class="chat-avatar-img">
     </div>
 `;
