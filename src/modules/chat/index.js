bc33d8f8e9b91a51d20a6614e659e8b4f05e6156
Aleksandra Mihel <mihelaleksa@yandex.ru>
2024-07-14 12:45:55 +0400

    x

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -2,7 +2,6 @@ import Handlebars from "handlebars";
 import { tmpl } from "./chat.tmpl.js";
 import "./chat.scss";
 import { Link } from "../../components/links/index.js";
-<<<<<<< HEAD
 import { Avatarchat } from "./components/chat-avatar/index.js";
 
 import avatarchat from "../../../static/img/user3.png";
@@ -11,15 +10,6 @@ import avatarchat from "../../../static/img/user3.png";
 export const Chats = () => {
   return Handlebars.compile(tmpl)({
     avatarchat: Avatarchat({ url: avatarchat }),
-=======
-import { Avatar} from "./components/chat-avatar/index.js";
-
-import avatarchat from "../../../static/img/user2.png";
-
-export const Chats = () => {
-  return Handlebars.compile(tmpl)({
-    avatarchat: Avatar({ url: avatarchat }),
->>>>>>> main
     profileLink: Link({
       to: "/profile",
       className: "chats__profile-link",
